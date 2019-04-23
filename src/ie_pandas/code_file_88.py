5060901d9c050cba40785797f4426d364c74ee30
Jiachao Pan <jiachao.pan@student.ie.edu>
2019-04-23 14:25:41 +0200

    added the part of test for sum and median, also added the code for calling sum and median from code_file_88.py, please double check see if it works

--- src/ie_pandas/code_file_88.py
+++ src/ie_pandas/code_file_88.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def sum(self):
+def sum_df(self):
     return np.array([self.df[col].sum() if col in self.numericals else None for col in self.column_names])
-def median(self):
+def median_df(self):
     return np.array([np.median(self.df[col]) if col in self.numericals else None for col in self.column_names])
-def min(self):
+def min_df(self):
     return np.array([self.df[col].min() if col in self.numericals else None for col in self.column_names])
-def max(self):
+def max_df(self):
     return np.array([self.df[col].max() if col in self.numericals else None for col in self.column_names])
