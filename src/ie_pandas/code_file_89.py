b87b1bd42449117d3e278879884bc6ef3ac6cb9c
Léon Junique <leon.kenske@gmail.com>
2019-04-23 16:37:59 +0200

    added a mean() and percentile() functions

--- src/ie_pandas/code_file_89.py
+++ src/ie_pandas/code_file_89.py
@@ -1,1 +1,27 @@
-print("Hello World")
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+
+
+def mean(self):
+    return np.array(
+        [
+            np.mean(self.df[col]) if col in self.numericals else None
+            for col in self.column_names
+        ]
+    )
+
+
+def percentile(self, x):
+    for x in range(1, 100):
+        return np.array(
+            [
+                np.percentile(self.df[col], x) if col in self.numericals else None
+                for col in self.column_names
+            ]
+        )
+
