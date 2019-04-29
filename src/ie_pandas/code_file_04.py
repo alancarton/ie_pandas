dcd7ce73b974bc7c2bb0fb99a0eeb7f6eea6d764
Federico Loguercio <f.loguercio@student.ie.edu>
2019-04-29 19:25:07 +0200

    Included get_row functionality with tests.

--- src/ie_pandas/code_file_04.py
+++ src/ie_pandas/code_file_04.py
@@ -37,14 +37,149 @@ def std(self):
     )
 
 
-# Create the test first
-# def test_head():
-# 	dataframe =
+def get_row(self, rowindex=None):
+    data = self.data
+
+    if not isinstance(rowindex, int):
+        raise TypeError("Row index must be an integer.")
+
+    if rowindex is None:
+        raise ValueError("Must specify row index.")
+
+    if abs(rowindex) > len(data):
+        raise ValueError("Row index out of range")
+
+    d_temp = {}
+
+    for key in data.keys():
+        d_temp[key] = data.get(key)[rowindex]
+
+    return list(d_temp.values())
 
 
-# def head(self, l=6):
-# 	return np.array(
-# 		[
-# 			self.df[1:l] if l>=
-# 		]
-# 	)
+from ie_pandas import DataFrame
+import numpy as np
+
+
+def test_getrow():
+    # Test whether get_row specifying the 4th row
+    # returns the 4th row
+
+    _data = np.array(
+        [
+            [1, 1.3254, False, "R1"],
+            [3, 4.123, True, "R 2"],
+            [3, 1.4, False, "R 3"],
+            [2, 14.0, False, "R 4"],
+            [12, 41.0, True, "R5"],
+        ]
+    )
+
+    _columns = ["IntCol", "FloatCol", "BoolCol", "StringCol"]
+    _index = [0, 1, 2, 3, 4]
+
+    df = DataFrame(data=_data, index=_index, columns=_columns)
+    row = 3
+    expected_output = [2, 14.0, False, "R 4"]
+    output = df.get_row(row)
+
+    # Test if created object equals the desired object
+    assert np.array_equal(output, expected_output)
+
+
+def test_get_negative_row():
+    # Tested behaviour: Return row counting from the end
+    # when specifying a negative row key.
+
+    _data = np.array(
+        [
+            [1, 1.3254, False, "R1"],
+            [3, 4.123, True, "R 2"],
+            [3, 1.4, False, "R 3"],
+            [2, 14.0, False, "R 4"],
+            [12, 41.0, True, "R5"],
+        ]
+    )
+
+    _columns = ["IntCol", "FloatCol", "BoolCol", "StringCol"]
+    _index = [0, 1, 2, 3, 4]
+
+    df = DataFrame(data=_data, index=_index, columns=_columns)
+    row = -1
+    expected_output = [12, 41.0, True, "R5"]
+    output = df.get_row(row)
+
+    # Test if created object equals the desired object
+    assert np.array_equal(output, expected_output)
+
+
+def test_get_row_No_Params():
+    # Test whether get_row function returns a ValueError
+    # when specifying no row
+
+    # Use pytest structure for testing the error-handling
+    with pytest.raises(ValueError) as ValError:
+        _data = np.array(
+            [
+                [1, 1.3254, False, "R1"],
+                [3, 4.123, True, "R 2"],
+                [3, 1.4, False, "R 3"],
+                [2, 14.0, False, "R 4"],
+                [12, 41.0, True, "R5"],
+            ]
+        )
+
+        _columns = ["IntCol", "FloatCol", "BoolCol", "StringCol"]
+        _index = [0, 1, 2, 3, 4]
+
+        output = df.get_row()
+
+
+def test_get_row_out_of_range():
+    # Tests whether get_row function raises a ValueError
+    # when specifying a row out of range
+
+    # Use pytest structure for testing the error-handling
+    with pytest.raises(ValueError) as ValError:
+
+        _data = np.array(
+            [
+                [1, 1.3254, False, "R1"],
+                [3, 4.123, True, "R 2"],
+                [3, 1.4, False, "R 3"],
+                [2, 14.0, False, "R 4"],
+                [12, 41.0, True, "R5"],
+            ]
+        )
+
+        _columns = ["IntCol", "FloatCol", "BoolCol", "StringCol"]
+        _index = [0, 1, 2, 3, 4]
+        row = 5
+
+        output = df.get_row(row)
+
+
+def test_get_row_string_rowkey():
+    # Tests whether get_row function raises a TypeError
+    # when specifying a string as row key.
+
+    # Use pytest structure for testing the error-handling
+    with pytest.raises(TypeError) as TypError:
+
+        _data = np.array(
+            [
+                [1, 1.3254, False, "R1"],
+                [3, 4.123, True, "R 2"],
+                [3, 1.4, False, "R 3"],
+                [2, 14.0, False, "R 4"],
+                [12, 41.0, True, "R5"],
+            ]
+        )
+
+        _columns = ["IntCol", "FloatCol", "BoolCol", "StringCol"]
+        _index = [0, 1, 2, 3, 4]
+
+        # Specify string as row index
+        row = "a"
+
+        output = df.get_row(row)
