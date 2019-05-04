94951459d293bd503b12c9ba5fffef836bd5f279
Alan Carton <alan.carton@student.ie.edu>
2019-05-04 10:57:49 +0100

    Adding more Column Test to Values Checks.

--- tests/test_values.py
+++ tests/test_values.py
@@ -44,3 +44,37 @@ def test_return_values_02():
     ]
 
     assert np.array_equal(df.to_array([0, 1, 2, 3]), expected)
+
+
+def test_return_values_03():
+
+    # itialization works, testing that the values function is as expected.
+    df = DataFrame(data=_dataarray1, columns=_columns)
+    # Can reorganize and specify columns by position
+    expected = [
+        ["1.3254", "1", "R1", "False"],
+        ["4.123", "3", "R 2", "True"],
+        ["1.4", "3", "R 3", "False"],
+        ["14.0", "2", "R 4", "False"],
+        ["41.0", "12", "R5", "True"],
+    ]
+
+    assert np.array_equal(df.to_array([1, 0, 3, 2]), expected)
+
+
+def test_return_values_04():
+
+    # itialization works, testing that the values function is as expected.
+    df = DataFrame(data=_dataarray1, columns=_columns)
+    # Can reorganize and specify columns by column name
+    expected = [
+        ["1.3254", "1", "R1", "False"],
+        ["4.123", "3", "R 2", "True"],
+        ["1.4", "3", "R 3", "False"],
+        ["14.0", "2", "R 4", "False"],
+        ["41.0", "12", "R5", "True"],
+    ]
+
+    assert np.array_equal(
+        df.to_array(["FloatCol", "IntCol", "StringCol", "BoolCol"]), expected
+    )
